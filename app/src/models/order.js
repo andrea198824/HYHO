e33e99572be4ffed4061e992951fafc49359c663
Andrea Gutierrez <88991918+andrea198824@users.noreply.github.com>
2022-03-03 07:16:08 -0500

    Update index.js (#4)
    * Update index.js
    
    * app
    
    * app
    
    * app

---
+++ app/src/models/order.js
@@ -0,0 +1,19 @@
+
+const { DataTypes, DatabaseError } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define("order", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    product: {
+      type: DataTypes.STRING,
+      allownull: false,
+      ref:"product",
+    },
+});
+};
