dbef752a32d0566f9690dbbaf46d6e1ed654a03b
Franco Leyes <84664199+Frsel99@users.noreply.github.com>
2022-03-03 16:50:44 -0300

    Navbar 1.0

--- client/src/components/Navbar/navbar.jsx
+++ client/src/components/Navbar/navbar.jsx
@@ -1,9 +1,35 @@
+import { useState } from "react";
+import { Link } from 'react-router-dom'
+
 
 const NavBar = () => {
+    const [search, setSearch] = useState("")
+
+
+    const onChangeSearch = (e) => {
+        setSearch(e.target.value)
+    }
+
+    const handleSubmitSearch = (e) => {
+        e.preventDefault()
+        // Aqui se hace el dispatch
+    }
+
     return (
-        <div>
-            test
-        </div>
+        <nav>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/a/ab/Logo_TV_2015.png" alt="404 Not Found" />
+            <form onSubmit={handleSubmitSearch}>
+                <input type="text" value={search} placeholder="Buscar Producto..." onChange={onChangeSearch} ></input>
+                <button>Buscar</button>
+            </form>
+            <Link to='/login'>
+                <button> Login/Registrarse </button>
+            </Link>
+            <Link to='/logout'>
+                <button> Logout </button>
+            </Link>
+            <input type="image" src="https://w7.pngwing.com/pngs/618/1013/png-transparent-shopping-cart-online-shopping-empty-cart-angle-logo-shopping-centre.png" alt="404 Not Found" />
+        </nav>
     )
 }
 
