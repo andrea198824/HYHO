34ab972222f6ea0890f2cea3c6fcbf654abdb30b
Andrea Gutierrez <88991918+andrea198824@users.noreply.github.com>
2022-03-03 18:56:44 -0500

    get-product (#40)

---
+++ app/src/routes/product.js
@@ -0,0 +1,52 @@
+const { Router } = require('express');
+const { Admin, Category, Form, order, Products, User } = require('../db');
+// Importar todos los routers;
+// Ejemplo: const authRouter = require('./auth.js');
+
+const router = Router();
+
+const getDbInfo = async () => {
+    return await Products.findAll({
+        include: {
+            model: Category,
+            attributes: ['name'],
+            through: {
+                attributes: [],
+            }
+        },
+    })
+}
+
+router.get("/products", async (req, res, next) => {
+    try {
+        const {name} = req.query;
+        let bdTotal = await getDbInfo(); 
+        if (name) {
+            // si hay un nombre por query
+            let prodName = await bdTotal.filter((product) =>
+            product.name.toLowerCase().includes(name.toLowerCase())
+            );
+            prodName.length //si hay algún nombre
+                ? res.status(200).send(prodName)
+                : res
+                    .status(404)
+                    .send({ info: "Sorry, the product you are looking for is not here." });
+        } else {
+            res.status(200).send(bdTotal); 
+        }
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.get('/products/:id', async (req, res) => {
+    const { id } = req.params
+    const findProduct = await getDbInfo()()
+    if (id) {
+        let productId = await findProduct.filter(x => x.id == id)
+        productId.length ?
+            res.status(200).send(productId) :
+            res.status(404).send('The product is not found in the system');
+
+    }
+})
