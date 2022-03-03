1f1402850de8cc219b63c60269da45428199e7dc
NachoEuclides <48378373+TuMandado@users.noreply.github.com>
2022-03-03 16:26:17 -0300

    Update productForm.js (#22)

--- app/src/models/productForm.js
+++ app/src/models/productForm.js
@@ -1,1 +1,34 @@
-//comentario
+
+const { DataTypes, DatabaseError } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define("productForm", {
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      price: {
+        type: DataTypes.FLOAT,
+        allowNull: false
+      },
+      weight: {
+        type: DataTypes.FLOAT,
+        allowNull: false
+      },
+      descriptions: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+      image: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+      stock: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+});
+};
+
