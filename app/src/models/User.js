2766238e75d525d6221d6d93ae929f7b0dde7943
Jennifer Lombardo <jenniferxlombardo@gmail.com>
2022-03-03 09:26:36 -0300

    Feature model users (#6)
    * Create User.js
    
    * Update User.js

---
+++ app/src/models/User.js
@@ -0,0 +1,48 @@
+const { DataTypes } = require('sequelize')
+
+module.exports = sequelize => {
+  sequelize.define('user', {
+    // id: {
+    //   type: DataTypes.INTEGER
+    // },
+    fullName: {
+      type: DataTypes.ENUM('first', 'last'),
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      validate: { isEmail: true },
+      unique: true
+    },
+    password: {
+      type: DataTypes.STRING(64),
+      validate: {
+        is: /^[0-9a-f]{64}$/i
+      }
+    },
+    billing_address: {
+      type: DataTypes.ENUM(
+        'streetName',
+        'streetNumber',
+        'aptNumber',
+        'city',
+        'provincia',
+        'country'
+      )
+    },
+    shipping_address: {
+      type: DataTypes.ENUM(
+        'streetName',
+        'streetNumber',
+        'aptNumber',
+        'city',
+        'provincia',
+        'country'
+      )
+    },
+    phone: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  })
+}
