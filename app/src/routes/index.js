06060b0621f8abd6abde31b44fab94d37e15ab69
andrea198824 <andreguti88@gmail.com>
2022-03-03 06:51:52 -0500

    app

---
+++ app/src/routes/index.js
@@ -0,0 +1,12 @@
+const { Router } = require('express');
+const { Pokemon, Type } = require('../db');
+const axios = require('axios');
+require("dotenv").config();
+// Importar todos los routers;
+// Ejemplo: const authRouter = require('./auth.js');
+
+
+const router = Router();
+
+// Configurar los routers
+// Ejemplo: router.use('/auth', authRouter);
