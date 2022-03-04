a96c459553d95aeb8cbdfda1c96543a253e4f51a
NachoEuclides <nachoeuclides@outlook.com>
2022-03-04 11:49:29 -0300

    Admin.js created

--- app/src/routes/index.js
+++ app/src/routes/index.js
@@ -4,6 +4,7 @@ const { Admin, Category, Form, Order, Products, User } = require('../db');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 var form = require('./form');
+var admin = require('./admin');
 
 const router = Router();
 
@@ -11,6 +12,10 @@ const router = Router();
 //Form endpoints:
 router.get("/donate-products", form.get); //Tested: Ok
 router.post('/donate-form', form.post); //Tested: Ok
+
+//Form endpoints:
+router.get("/admins", admin.get); //Tested: Ok
+router.post('/create-admin', admin.post); //Tested: Ok
 
 
 
