b565e5ed98b0f831bb160b6ab7240f19764b647f
andrea198824 <andreguti88@gmail.com>
2022-03-03 22:26:32 -0500

    get product-form

--- app/src/routes/product.js
+++ app/src/routes/product.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const { Admin, Category, Form, order, Products, User } = require('../db');
+const { Admin, Category, Products } = require('../db');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -19,12 +19,12 @@ const getDbInfo = async () => {
 
 router.get("/products", async (req, res, next) => {
     try {
-        const {name} = req.query;
+        const {fullName} = req.query;
         let bdTotal = await getDbInfo(); 
-        if (name) {
-            // si hay un nombre por query
+        if (fullName) {
+
             let prodName = await bdTotal.filter((product) =>
-            product.name.toLowerCase().includes(name.toLowerCase())
+            product.fullName.toLowerCase().includes(fullName.toLowerCase())
             );
             prodName.length //si hay algún nombre
                 ? res.status(200).send(prodName)
