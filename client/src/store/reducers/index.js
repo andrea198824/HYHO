239f7569aa43336e1b2e43a085e9e3770d21f7a9
Franco Leyes <84664199+Frsel99@users.noreply.github.com>
2022-03-04 16:10:13 -0300

    Añado Redux. (#67)
    * Routing Fixed
    
    * Añado global store con Redux

---
+++ client/src/store/reducers/index.js
@@ -0,0 +1,12 @@
+
+const initialState = {
+    products: [],
+
+}
+
+export default function rootReducer(state = initialState, action) {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
