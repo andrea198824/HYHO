45f99f8a6dc5d3ab77543eda265863e213716234
Luciana Zanetti <luciana.zeta@gmail.com>
2022-03-04 22:18:17 -0300

    Update productForm.js
    LIsto el post falta elmodificar index.js

--- app/src/routes/productForm.js
+++ app/src/routes/productForm.js
@@ -7,35 +7,37 @@ const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-router.post('/product/form', async (req, res, next) => {
 
 
-const {fullName, price, weight, descriptions, image, stock, available, category} = body;
-//console.log(req.body)
-try{
-    const [newProduct, created] = await Products.create({
-      
-      where: {
-            fullName,
-            price,
-            weight,
-            descriptions,
-            image,
-            stock,
-            available,
-      }
-    });
-    console.log(created);
+// router.post('/product/form', async (req, res, next) => {
 
-    if(category){
-      let categoryA = await Category.findOrCreated({where: {name: category}});
-      await newProduct.addCategory(categoryA)
-    }
-    res.status(200).send(`El producto ${fullName} creado con exito`)
-} catch (error) {
-    next(error)
-}
-})
+
+//     const {fullName, price, weight, descriptions, image, stock, avaliable, category} = req.body;
+//     console.log(req.body)
+//     try{
+//         let [newProduct, created] = await Products.create({
+          
+//           where: {
+//                 fullName,
+//                 price,
+//                 weight,
+//                 descriptions,
+//                 image,
+//                 stock,
+//                 avaliable,
+//           },
+//         });
+//         console.log(created);
+    
+//         if(category){
+//           let categoryA = await Category.findOrCreate({where: {name: category}});
+//           await newProduct.addCategory(categoryA)
+//         }
+//         res.status(200).send(`El producto ${fullName} creado con exito`)
+//     } catch (error) {
+//         next(error)
+//     }
+//     })
 
 
 module.exports = router;
