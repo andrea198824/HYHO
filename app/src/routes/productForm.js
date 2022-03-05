7b20703a31ad8b234d8cfa8ef7f56424023dbe10
Luciana Zanetti <luciana.zeta@gmail.com>
2022-03-05 09:14:49 -0300

    update index
    update index.js

--- app/src/routes/productForm.js
+++ app/src/routes/productForm.js
@@ -1,41 +1,39 @@
-const { Router } = require('express');
+'use strict'
 const { Category, Products } = require('../db');
-// Importar todos los routers;
-// Ejemplo: const authRouter = require('./auth.js');
-const axios = require("axios");
-const router = Router();
+
+
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-router.post('/product/form', async (req, res, next) => {
+exports.post = async function(req, res, next) {
 
-
-const {fullName, price, weight, descriptions, image, stock, available, category} = body;
-//console.log(req.body)
-try{
-    const [newProduct, created] = await Products.create({
+  const {fullName, price, weight, descriptions, image, stock, avaliable, category} = req.body;
+  
+  try {
+      const newCategory = await Category.findOrCreate({
+        
+        where: {
+          name: category
+        },
+      });
       
-      where: {
-            fullName,
-            price,
-            weight,
-            descriptions,
-            image,
-            stock,
-            available,
+  
+      if(fullName, price, weight, descriptions, image, stock){
+        await Products.create({
+          fullName,
+          price,
+          weight,
+          descriptions,
+          image,
+          stock,
+          avaliable,
+        });
+       
       }
-    });
-    console.log(created);
-
-    if(category){
-      let categoryA = await Category.findOrCreated({where: {name: category}});
-      await newProduct.addCategory(categoryA)
-    }
-    res.status(200).send(`El producto ${fullName} creado con exito`)
-} catch (error) {
-    next(error)
-}
-})
+      res.status(200).send(`El producto ${fullName} creado con exito`)
+  } catch (error) {
+      next(error)
+  }
+  }
 
 
-module.exports = router;
