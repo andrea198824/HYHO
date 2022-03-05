9e73092a9949c33f7cded96ce345b314bd2476a8
Luciana Zanetti <luciana.zeta@gmail.com>
2022-03-05 07:17:42 -0300

    Update productForm.js
    modularizado productForm

--- app/src/routes/productForm.js
+++ app/src/routes/productForm.js
@@ -1,43 +1,36 @@
-const { Router } = require('express');
+'use strict'
 const { Category, Products } = require('../db');
-// Importar todos los routers;
-// Ejemplo: const authRouter = require('./auth.js');
-const axios = require("axios");
-const router = Router();
-
-// Configurar los routers
-// Ejemplo: router.use('/auth', authRouter);
 
 
-// router.post('/product/form', async (req, res, next) => {
+exports.post = async function(req, res, next) {
 
-
-//     const {fullName, price, weight, descriptions, image, stock, avaliable, category} = req.body;
-//     console.log(req.body)
-//     try{
-//         let [newProduct, created] = await Products.create({
+    const {fullName, price, weight, descriptions, image, stock, avaliable, category} = req.body;
+   
+    try {
+        const newCategory = await Category.findOrCreate({
           
-//           where: {
-//                 fullName,
-//                 price,
-//                 weight,
-//                 descriptions,
-//                 image,
-//                 stock,
-//                 avaliable,
-//           },
-//         });
-//         console.log(created);
+          where: {
+            name: category
+          },
+        });
+        
     
-//         if(category){
-//           let categoryA = await Category.findOrCreate({where: {name: category}});
-//           await newProduct.addCategory(categoryA)
-//         }
-//         res.status(200).send(`El producto ${fullName} creado con exito`)
-//     } catch (error) {
-//         next(error)
-//     }
-//     })
+        if(fullName, price, weight, descriptions, image, stock){
+          await Products.create({
+            fullName,
+            price,
+            weight,
+            descriptions,
+            image,
+            stock,
+            avaliable,
+          });
+         
+        }
+        res.status(200).send(`El producto ${fullName} creado con exito`)
+    } catch (error) {
+        next(error)
+    }
+    }
 
 
-module.exports = router;
