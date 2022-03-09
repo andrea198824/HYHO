32ac1cfe362be0b6c831d4c619d530672b547b1a
Franco Leyes <84664199+Frsel99@users.noreply.github.com>
2022-03-08 22:27:32 -0300

    Carrito v1.0 (#115)

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,8 +1,15 @@
-import {createStore, applyMiddleware, compose} from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducers/index';
 import thunk from 'redux-thunk';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+const persistedState = localStorage.getItem('reduxState') ? JSON.parse(localStorage.getItem('reduxState')) : {
+    products: [],
+    details: [],
+    categories: [],
+    filteredProducts: [],
+    shoppingCart: [],
+}
+const store = createStore(rootReducer, persistedState, composeEnhancers(applyMiddleware(thunk)));
 
 export default store;
--- client/src/store/reducers/index.js
+++ client/src/store/reducers/index.js
@@ -1,16 +1,15 @@
-import { GET_DETAILS, GET_PRODUCTS, SEARCH_PRODUCTS, ORDER_BY_PRICE, FILTER_BY_CATEGORY, GET_CATEGORIES } from "../actions";
+import { GET_DETAILS, GET_PRODUCTS, SEARCH_PRODUCTS, ORDER_BY_PRICE, FILTER_BY_CATEGORY, GET_CATEGORIES, ADD_TO_CART } from "../actions";
 
 const initialState = {
     products: [],
     details: [],
     categories: [],
     filteredProducts: [],
-
+    shoppingCart: [],
 }
 
 export default function rootReducer(state = initialState, action) {
-    switch (action.type) {
-
+        switch (action.type) {
         case GET_PRODUCTS:
             return { ...state, products: action.payload }
         case SEARCH_PRODUCTS:
@@ -23,16 +22,19 @@ export default function rootReducer(state = initialState, action) {
             }
 
             if (action.payload === 'asc') {
-                return { ...state, filteredProducts: [...state.filteredProducts].sort((prev, next) => next.price - prev.price), products: [...state.products].sort((prev, next) => next.price - prev.price) }
+                return { ...state, filteredProducts: [...state.filteredProducts].sort((prev, next) => next.price - prev.price), products: [...state.products].sort((prev, next) => next.price - prev.price) };
             }
             break;
         case GET_CATEGORIES:
-            return { ...state, categories: action.payload }
+            return { ...state, categories: action.payload };
         case FILTER_BY_CATEGORY:
-            if(action.payload === "default"){
-                return {...state, filteredProducts: state.products}
+            if (action.payload === "default") {
+                return { ...state, filteredProducts: state.products };
             }
-            return { ...state, filteredProducts: state.products.filter(product => product.category === action.payload) }
+            return { ...state, filteredProducts: state.products.filter(product => product.category === action.payload) };
+        case ADD_TO_CART:
+            if (state.shoppingCart.some(el => el.id === action.payload)) return state;
+            return { ...state, shoppingCart: state.shoppingCart.concat(state.products.filter(product => product.id === action.payload)) };
         default:
             return state;
     }
