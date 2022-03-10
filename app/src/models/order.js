95d806c500935296ecb43163c8f2a563d63353ab
jennyxlombardo <78739007+jennyxlombardo@users.noreply.github.com>
2022-03-10 12:33:17 -0300

    shopOrder

--- app/src/models/order.js
+++ app/src/models/order.js
@@ -1,39 +1,20 @@
-
-const { DataTypes, DatabaseError } = require('sequelize');
+const { DataTypes, DatabaseError } = require('sequelize')
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
-module.exports = (sequelize) => {
+module.exports = sequelize => {
   // defino el modelo
-  sequelize.define("order", {
-    productQuantity: {
-      type: DataTypes.ARRAY (DataTypes.JSON), 
-      allowNull: false,
-      ref:"product",
-      // Ejemplo
-      // [
-//     {
-//       product: "product 1",
-//       price: 2
-//     },
-//       {
-//       product: "product 2",
-//       price: 22
- //      quantity: 2
-//     },
-//       {
-//       product: "product 3",
-//       price: 6.7
-//     },
-//   ]
+  sequelize.define('order', {
+    order: {
+      type: DataTypes.STRING,
+      allowNull: false
     },
-    
     status: {
-        type: DataTypes.ENUM('Active','Finalized'),
-        allowNull: true,
+      type: DataTypes.ENUM('Active', 'Cancelled', 'Finalized'),
+      allowNull: true
     },
     total: {
-        type: DataTypes.INTEGER,
+      type: DataTypes.INTEGER,
+      allowNull: false
     }
-});
-};
-
+  })
+}
