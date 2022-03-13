00376c835a82e2d7a62ab863dcc33311dff0a34d
Lion.Fernandez <leonardojulianfernandez@gmail.com>
2022-03-12 23:13:07 -0300

    video

--- client/src/components/Slider.jsx
+++ client/src/components/Slider.jsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import { sliderItems } from "../data";
 import { mobile } from "../responsive";
 import { Link } from 'react-router-dom';
+import videoLanding from  "../Img/landing_verde.mp4";
 
 
 const Div = styled.div`
@@ -52,20 +53,31 @@ const Slide = styled.div`
   width: 100vw;
   height: 100vh;
   display: flex;
+  justify-content: center;
   align-items: center;
+  align-content: center;
   background-color: #${(props) => props.bg};
 `;
 
 const ImgContainer = styled.div`
-  height: 100%;
-  flex: 1;
+   
+  width: 100%;
+  flex: 2 ;
+  align-self: stretch
 `;
 
 const Image = styled.img`
   height: 80%;
+  width: 100%;
+`;
+
+const Video = styled.video`
+height: 72%;
+margin-top:-180px;
 `;
 
 const InfoContainer = styled.div`
+
   flex: 1;
   padding: 50px;
 `;
@@ -96,9 +108,9 @@ const Slider = () => {
   const [slideIndex, setSlideIndex] = useState(0);
   const handleClick = (direction) => {
     if (direction === "left") {
-      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
+      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 3);
     } else {
-      setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
+      setSlideIndex(slideIndex < 3 ? slideIndex + 1 : 0);
     }
   };
 
@@ -113,6 +125,8 @@ const Slider = () => {
       <Wrapper slideIndex={slideIndex}>
         {sliderItems.map((item) => (
           <Slide bg={item.bg} key={item.id}>
+            { item.video ? <Video autoplay controls src={videoLanding} type="video/mp4"/>:
+            <>
             <ImgContainer>
               <Image src={item.img[0]} />
             </ImgContainer>
@@ -123,6 +137,8 @@ const Slider = () => {
                 <Button>Saber +</Button>
               </Link>
             </InfoContainer>
+            </>
+            }
           </Slide>
         ))}
       </Wrapper>
@@ -131,7 +147,7 @@ const Slider = () => {
       </Arrow>
     </Container>
     </Div>
-  );
+    );
 };
 
 export default Slider;
