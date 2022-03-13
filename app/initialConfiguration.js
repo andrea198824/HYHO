0aaafaa1443f2d679fe4fa97398654802a6caab1
NachoEuclides <nachoeuclides@outlook.com>
2022-03-13 15:03:57 -0300

    Update initialConfiguration.js

--- app/initialConfiguration.js
+++ app/initialConfiguration.js
@@ -7,15 +7,20 @@ let securityString = "randomString"
 let password = "12345678"
 password = CryptoJS.HmacSHA1(securityString, password).toString(CryptoJS.enc.Base64)
 exports.do = async () => {
-    await User.create({
-      fullName:"Admin",
-      email:"Admin@gmail.com",
-      password,
-      securityString,
-      emailVerificated:true,
-      adminVerificated:true,
-      billing_address:"",
-      shipping_address:"",
-      phone:"",
-    });
+    let user = await User.findAll({
+      where: {id: 1}
+    })
+    if (!user.length) {
+      await User.create({
+        fullName:"Admin",
+        email:"Admin@gmail.com",
+        password,
+        securityString,
+        emailVerificated:true,
+        adminVerificated:true,
+        billing_address:"",
+        shipping_address:"",
+        phone:"",
+      });
+    }
   }
