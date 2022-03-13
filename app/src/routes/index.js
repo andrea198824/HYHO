49f2ca085f92f073b1001ab3ad249e2b8eec02f7
NachoEuclides <nachoeuclides@outlook.com>
2022-03-13 16:11:47 -0300

    uncomment get products

--- app/src/routes/index.js
+++ app/src/routes/index.js
@@ -52,7 +52,7 @@ router.get('/newsletter', newsletter.get)
 const shopController = require('./shop')
 
 // router.get('/', shopController.getIndex);
-// router.get('/shop' ,shopController.getProducts) //  -> Usar /products
+router.get('/shop' ,shopController.getProducts) //  -> Usar /products
 router.get('/cart', sessionVerification.needsUser ,shopController.getCart)
 router.post('/cart', sessionVerification.needsUser ,shopController.postCart)
 // router.get('/checkout', shopController.getCheckout);
