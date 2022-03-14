30f03ce82a071a019738cd6bacdb13199a6bfb52
NachoEuclides <nachoeuclides@outlook.com>
2022-03-14 10:52:36 -0300

    deploy

--- app/src/db.js
+++ app/src/db.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const path = require('path');
 const { userInfo } = require('os');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,
+  DB_USER, DB_PASSWORD, DB_HOST,DB_NAME
 } = process.env;
 
 let sequelize =
