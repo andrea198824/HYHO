72eb38e832ac8a084222821fb539db221e7211f3
NachoEuclides <nachoeuclides@outlook.com>
2022-03-17 17:49:14 -0300

    Update user and deletes admin from initial configuration.

--- app/initialConfiguration.js
+++ app/initialConfiguration.js
@@ -163,22 +163,22 @@ let securityString = "randomString"
 let password = "12345678"
 password = CryptoJS.HmacSHA1(securityString, password).toString(CryptoJS.enc.Base64)
 exports.do = async () => {
-    let user = await User.findAll({
-      where: {id: 1}
-    })
-    if (!user.length) {
-      await User.create({
-        fullName:"Admin",
-        email:"Admin@gmail.com",
-        password,
-        securityString,
-        emailVerificated:true,
-        adminVerificated:true,
-        billing_address:"",
-        shipping_address:"",
-        phone:"",
-      });
-    }
+    // let user = await User.findAll({
+    //   where: {id: 1}
+    // })
+    // if (!user.length) {
+    //   await User.create({
+    //     fullName:"Admin",
+    //     email:"Admin@gmail.com",
+    //     password,
+    //     securityString,
+    //     emailVerificated:true,
+    //     adminVerificated:true,
+    //     billing_address:"",
+    //     shipping_address:"",
+    //     phone:"",
+    //   });
+    // }
     // let dbProducts = await Products.findAll();
     if (true) {
       await Promise.all(
