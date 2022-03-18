f2ed4a0a5a4b6e6d53b366f79d0d6dcc83051949
José Antonio <josea_@hotmail.com>
2022-03-18 01:33:31 -0300

    ok

--- client/src/components/Maps.jsx
+++ client/src/components/Maps.jsx
@@ -7,20 +7,26 @@ import {
 } from 'react-google-maps'
 import { Link } from 'react-router-dom'
 
-const Map = props => {
+function Map({ lat, lng }) {
+    
   return (
-    <div>
-      <GoogleMap
-        defaultZoom={15}
-        defaultCenter={{ lat: -34.6037851, lng: -58.381775 }}
-      ></GoogleMap>
+      <div>
+          <GoogleMap
+              defaultZoom={15}
+              defaultCenter={{ lat, lng }}
+          ></GoogleMap>
 
 
-      <Marker position={{ lat: -34.6037851, lng: -58.381775 }}></Marker>
-      <p>Podes encontrarnos en Av. 9 de Julio, C1043 CABA, Argentina</p>
-      <Link to='/'>Back</Link>
-    </div>
+          <Marker
+              draggable={true}
+              position={{ lat, lng }}
+          >
+          </Marker>
+          
+          <p>Podes encontrarnos en Av. 9 de Julio, C1043 CABA, Argentina</p>
+          <Link to='/'>Back</Link>
+      </div>
   )
 }
-
+//lat: -34.6037851, lng: -58.381775
 export default withScriptjs(withGoogleMap(Map))
