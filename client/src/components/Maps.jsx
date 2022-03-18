0830a53f1c0e2e1a4b2584984947dfcadbaa7b16
NachoEuclides <nachoeuclides@outlook.com>
2022-03-18 16:36:33 -0300

    Maps updated

--- client/src/components/Maps.jsx
+++ client/src/components/Maps.jsx
@@ -7,24 +7,25 @@ import {
 } from 'react-google-maps'
 import { Link } from 'react-router-dom'
 
-function Maps () {
+
+function Maps() {
   return (
     <div>
-      {/* <GoogleMap
-        defaultZoom={15}
-        defaultCenter={{ lat: -34.6037851, lng: -58.381775 }}
-      ></GoogleMap>
+       <GoogleMap
+         defaultZoom={15}
+         defaultCenter={{ lat: -34.6037851, lng: -58.381775 }}
+       ></GoogleMap>
 
-      <Marker
-        draggable={true}
-        position={{ lat: -34.6037851, lng: -58.381775 }}
+       <Marker
+         draggable={true}
+         position={{ lat: -34.6037851, lng: -58.381775 }}
       ></Marker>
 
-      // <p>Podes encontrarnos en Av. 9 de Julio, C1043 CABA, Argentina</p>
-      // <Link to='/'>Back</Link> */}
-      hola mapa
-    </div>
+      <p>Podes encontrarnos en Av. 9 de Julio, C1043 CABA, Argentina</p>
+      <Link to='/'>Back</Link>
+      </div>
   )
 }
-//lat: -34.6037851, lng: -58.381775
-export default withScriptjs(withGoogleMap(Map))
+
+// //lat: -34.6037851, lng: -58.381775
+export default withScriptjs(withGoogleMap(Maps))
