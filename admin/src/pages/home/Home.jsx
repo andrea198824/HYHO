6bcba0fe2b9ba627737e29f83ec7034c7fe65e1e
Leonardo Fernandez <leonardojulianfernandez@gmail.com>
2022-03-19 19:56:08 -0300

    admin Store

--- admin/src/pages/home/Home.jsx
+++ admin/src/pages/home/Home.jsx
@@ -1,18 +1,31 @@
+import React from "react";
 import Chart from "../../components/chart/Chart";
 import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
-import "./home.css";
-import { userData } from "../../dummyData";
 import WidgetSm from "../../components/widgetSm/WidgetSm";
-import WidgetLg from "../../components/widgetLg/WidgetLg";
+// import WidgetLg from "../../components/widgetLg/WidgetLg";
+import { userData } from "../../dummyData";
+import {useAuth0 } from '@auth0/auth0-react';
+import { getToken } from "../../store/actions";
+import { useDispatch } from "react-redux";
+import "./home.css";
+
 
 export default function Home() {
+
+const dispatch = useDispatch();
+const {getAccessTokenSilently, isLoading} = useAuth0();
+if (!isLoading) {
+  getAccessTokenSilently()
+  .then( (res)=> dispatch(getToken(res)))
+} 
+
   return (
     <div className="home">
       <FeaturedInfo />
-      <Chart data={userData} title="User Analytics" grid dataKey="Active User"/>
+      <Chart data={userData} title="Analisis de Mercado" grid dataKey="Active User"/>
       <div className="homeWidgets">
-        {/* <WidgetSm/> */}
-        <WidgetLg/>
+        {/* <WidgetLg/> */}
+        <WidgetSm/>
       </div>
     </div>
   );
