b0c8fa0e4c34639565c003221d9c3ba878683f9f
NachoEuclides <48378373+TuMandado@users.noreply.github.com>
2022-03-22 18:32:41 -0300

    donar producto actualizado (#223)

--- app/src/routes/form.js
+++ app/src/routes/form.js
@@ -18,7 +18,9 @@ exports.post = async function(req, res){  // lalala.post
             image,
             stock,
           } = req.body;
+          console.log(req.body)
 
+<<<<<<< Updated upstream
     if (!title) {
         res.send({info: "No title"});
         return
@@ -39,14 +41,43 @@ exports.post = async function(req, res){  // lalala.post
         res.send({info: "No image"});
         return
         }
+=======
+    // if (!title) {
+    //     res.status(404).send({info: "No title"});
+    //     return
+    //     }
+    // if (!price) {
+    //     res.status(404).send({info: "No price"});
+    //     return
+    //     }
+    // if (!weight) {
+    //     res.status(404).send({info: "No weight"});
+    //     return
+    //     }
+    // if (!descriptions) {
+    //     res.status(404).send({info: "No descriptions"});
+    //     return
+    //     }
+    // if (!image) {
+    //     res.status(404).send({info: "No image"});
+    //     return
+    //     }
+>>>>>>> Stashed changes
 
     stock = stock ? stock : 1;
 
 
+<<<<<<< Updated upstream
     if (!email) {
         res.send({info: "No email"});
         return
         }
+=======
+    // if (!email) {
+    //     res.status(404).send({info: "No email"});
+    //     return
+    //     }
+>>>>>>> Stashed changes
     
     const formCreated = await Form.create({
       title,
@@ -68,9 +99,11 @@ exports.post = async function(req, res){  // lalala.post
         formCreated
       );
     }).catch((err) => {
+        console.log("err    :",err)
         res.status(404).send({info: err});
     })
     } catch (error) {
+        console.log("error    :",error)
         res.status(404).send(error)
     }
      
