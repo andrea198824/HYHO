42ce048331e6b3b32401e1bb045c68515c6ad2bf
jennyxlombardo <78739007+jennyxlombardo@users.noreply.github.com>
2022-03-23 09:29:48 -0300

    Create Counter.jsx

---
+++ client/src/components/Counter.jsx
@@ -0,0 +1,24 @@
+import { React } from 'react'
+import CountUp from 'react-countup'
+
+const donation = {
+  display: 'flex',
+  fontSize: '26px',
+  paddingBottom: '15px',
+  paddingLeft: '10px',
+  paddingTop: '12px',
+  color: '#8aa290',
+  textAlign: 'center',
+  flexWrap: 'nowrap'
+}
+
+
+const Counter = () => {
+  return (
+    <div style={donation}>
+      <p>Fondos recuadado para: $</p>
+      <CountUp start={0} end={4568220} />
+    </div>
+  )
+}
+export default Counter
