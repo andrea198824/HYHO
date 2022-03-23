06e739e3879605104e7d10b0f56ae1106141c27c
NachoEuclides <nachoeuclides@outlook.com>
2022-03-23 20:08:55 -0300

    Chart updated

--- client/src/components/chart/Chart.jsx
+++ client/src/components/chart/Chart.jsx
@@ -7,8 +7,103 @@ import {
   Tooltip,
   ResponsiveContainer,
 } from "recharts";
+import { useEffect, useState } from "react";
+import axios from 'axios';
+import { useSelector } from "react-redux";
 
 const Chart = ({ grid=grid }) => {
+  const token = useSelector(state => state.token)
+  let [date, setDate] = useState(new Date());
+  let [months, setMonths] = useState([]);
+  let [data2, setData2] = useState([]);
+
+  useEffect(()=>{
+    setMonths(setMonthsArray(date))
+      console.log("months :",months)
+  },[token])
+
+  useEffect(()=>{
+    if(token) {
+      createData(token)
+    }
+   },[token])
+
+   useEffect(()=>{
+    console.log("data2    :",data2)
+   },[data2])
+  
+  function setMonthsArray(today){
+    const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"];
+  let toReturn = []
+  let yearMinus = 0
+  let thisMonth = today.getMonth()
+  for (let i = thisMonth; i < 24; i--) {
+    if (!monthNames[i]) i = 11
+    if(i == 11) yearMinus = -1
+    	toReturn.push({
+        month: monthNames[i],
+        monthNumber: i,
+        yearMinus})
+    if (monthNames.length == toReturn.length) break
+  }
+  if (thisMonth == 11){
+    for (let i = 0; i < toReturn.length; i++) {
+      toReturn[i].yearMinus = 0
+    }
+  }
+  return toReturn
+  }
+
+  const getTotalVentas = async (year, month, token) => {
+    const response = await axios.post('/mercadopago/totalVentas',{
+      year: year,
+      month: month
+    }, {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },  
+    })
+    // console.log("response axios :",response)
+    return response.data.Total
+}
+
+const getTotalDonaciones = async (year, month, token) => {
+  const response = await axios.post('/totalDonation',{
+    year: year,
+    month: month
+  }, {
+      headers: {
+          Authorization: `Bearer ${token}`,
+      },  
+  })
+  // console.log("response axios :",response)
+  return response.data.Total
+}
+
+const createData = async (token) => {
+  let ventas
+  let donaciones
+  let total
+  let data2 = []
+  for (let i = 0; i < months.length; i++) {
+    ventas = await getTotalVentas(date.getFullYear()+months[i].yearMinus,months[i].monthNumber, token);
+    donaciones = await getTotalDonaciones(date.getFullYear()+months[i].yearMinus,months[i].monthNumber, token);
+    total = ventas + donaciones;
+    data2.push({
+      month: months[i].month,
+      monthNumber: months[i].monthNumber,
+      ventas,
+      donaciones,
+      total
+    })
+  }
+  console.log("toReturn :",data2)
+  setData2(data2)
+ }
+  
+
+
   let data = [
     {
       name: "Jan",
