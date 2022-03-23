efc95a815b80213b55f5245eee7681dcf9455575
NachoEuclides <nachoeuclides@outlook.com>
2022-03-23 20:16:18 -0300

    Update Chart.jsx

--- client/src/components/chart/Chart.jsx
+++ client/src/components/chart/Chart.jsx
@@ -99,70 +99,21 @@ const createData = async (token) => {
     })
   }
   // console.log("toReturn :",data2)
-  setData2(data2)
+  setData2(data2.reverse())
  }
   
 
-
-  let data = [
-    {
-      name: "Jan",
-      "Active User": 4000,
-    },
-    {
-      name: "Feb",
-      "Active User": 3000,
-    },
-    {
-      name: "Mar",
-      "Active User": 5000,
-    },
-    {
-      name: "Apr",
-      "Active User": 4000,
-    },
-    {
-      name: "May",
-      "Active User": 3000,
-    },
-    {
-      name: "Jun",
-      "Active User": 2000,
-    },
-    {
-      name: "Jul",
-      "Active User": 4000,
-    },
-    {
-      name: "Agu",
-      "Active User": 3000,
-    },
-    {
-      name: "Sep",
-      "Active User": 4000,
-    },
-    {
-      name: "Oct",
-      "Active User": 1000,
-    },
-    {
-      name: "Nov",
-      "Active User": 4000,
-    },
-    {
-      name: "Dec",
-      "Active User": 3000,
-    },
-  ];
   let title="Analisis de Mercado"
   let dataKey="Active User"
     return (
     <div className="chart">
       <h3 className="chartTitle">{title}</h3>
       <ResponsiveContainer width="100%" aspect={4 / 1}>
-        <LineChart data={data}>
-          <XAxis dataKey="name" stroke="#6a7c6e" />
-          <Line type="monotone" dataKey={dataKey} stroke="#6a7c6e" />
+        <LineChart data={data2}>
+          <XAxis dataKey="month" stroke="#6a7c6e" />
+          <Line type="monotone" dataKey="ventas" stroke="#6a7c6e" />
+          <Line type="monotone" dataKey="donaciones" stroke="#6a7c6e" />
+          <Line type="monotone" dataKey="total" stroke="#6a7c6e" />
           <Tooltip />
           {grid && <CartesianGrid stroke="#6a7c6e" strokeDasharray="5 5" />}
         </LineChart>
