9f9f6eee0f61ef36af09602a8d7a6c50b3e95724
José Antonio <josea_@hotmail.com>
2022-03-24 01:31:41 -0300

    hghngn

--- admin/src/components/sidebar/Sidebar.jsx
+++ admin/src/components/sidebar/Sidebar.jsx
@@ -17,74 +17,78 @@ import { Link } from "react-router-dom";
 
 
 export default function Sidebar() {
-  return (
-    <div className="sidebar">
-      <div className="sidebarWrapper">
-        <div className="sidebarMenu">
-          <h3 className="sidebarTitle">Tablero</h3>
-          <ul className="sidebarList">
-            <Link to="/" className="link">
-            <li className="sidebarListItem active">
-              <LineStyle className="sidebarIcon" />
-              Pagina principal
-            </li>
-            </Link>
-          </ul>
-        </div>
-        <div className="sidebarMenu">
-          <ul className="sidebarList">
-            <Link to="/users" className="link">
-              <li className="sidebarListItem">
-                <PermIdentity className="sidebarIcon" />
-                Usuarios
-              </li>
-            </Link>
-            {/* <Link to="https://hyho.vercel.app/register" className="link">
+    return (
+        <div className="sidebar">
+            <div className="sidebarWrapper">
+                <div className="sidebarMenu">
+                    <h3 className="sidebarTitle">Tablero</h3>
+                    <ul className="sidebarList">
+                        <Link to="/" className="link">
+                            <li className="sidebarListItem active">
+                                <LineStyle className="sidebarIcon" />
+                                Pagina principal
+                            </li>
+                        </Link>
+                    </ul>
+                </div>
+                <div className="sidebarMenu">
+                    <ul className="sidebarList">
+                        <Link to="/users" className="link">
+                            <li className="sidebarListItem">
+                                <PermIdentity className="sidebarIcon" />
+                                Usuarios
+                            </li>
+                        </Link>
+                        {/* <Link to="https://hyho.vercel.app/register" className="link">
               <li className="sidebarListItem">
                 <PermIdentity className="sidebarIcon" />
                 Nuevo Usuario
               </li>
             </Link> */}
-            <Link to="/products" className="link">
-              <li className="sidebarListItem">
-                <Storefront className="sidebarIcon" />
-                Productos
-              </li>
-            </Link>
-            <Link to="/newProduct" className="link">
-              <li className="sidebarListItem">
-                <Storefront className="sidebarIcon" />
-                Nuevo Producto
-              </li>
-            </Link>
-            <li className="sidebarListItem">
-              <AttachMoney className="sidebarIcon" />
-              ventas
-            </li>
-          </ul>
+                        <Link to="/products" className="link">
+                            <li className="sidebarListItem">
+                                <Storefront className="sidebarIcon" />
+                                Productos
+                            </li>
+                        </Link>
+                        <Link to="/newProduct" className="link">
+                            <li className="sidebarListItem">
+                                <Storefront className="sidebarIcon" />
+                                Nuevo Producto
+                            </li>
+                        </Link>
+                        <li className="sidebarListItem">
+                            <AttachMoney className="sidebarIcon" />
+                            ventas
+                        </li>
+                    </ul>
+                </div>
+                <div className="sidebarMenu">
+                    <h3 className="sidebarTitle">Donaciones</h3>
+                    <ul className="sidebarList">
+                        <Link to="/productos-donados" className="link" >
+                            <li className="sidebarListItem">
+                                <DynamicFeed className="sidebarIcon" />
+                                Formularios donaciones de productos
+                            </li>
+                        </Link>
+                    </ul>
+                    <div className="sidebarMenu">
+                        <h3 className="sidebarTitle">Newsletter</h3>
+                        <ul className="sidebarList">
+                            <Link to="/newsletter" className="link">
+                                <li className="sidebarListItem">
+                                    <MailOutline className="sidebarIcon" />
+                                    Mail
+                                </li>
+                            </Link>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className="sidebarMenu">
-          <h3 className="sidebarTitle">Donaciones</h3>
-          <ul className="sidebarList">
-          <Link to="/productos-donados" className="link" >
-            <li className="sidebarListItem">
-              <DynamicFeed className="sidebarIcon" />
-              Formularios donaciones de productos
-            </li>
-            </Link>
-        </div>
-        <div className="sidebarMenu">
-          <h3 className="sidebarTitle">Newsletter</h3>
-          <ul className="sidebarList">
-            <Link to="/newsletter" className="link">
-              <li className="sidebarListItem">
-                <MailOutline className="sidebarIcon" />
-                Mail
-              </li>
-            </Link>
-          </ul>
-        </div>
-      </div>
-    </div>
-  );
+
+            );
+        
+
 }
