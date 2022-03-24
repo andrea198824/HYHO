2651b23bae91219cc9f84dfcd355db9c14af2041
Jennifer Lombardo <jenniferxlombardo@gmail.com>
2022-03-24 11:25:56 -0300

    Arreglos/css/responsive (#267)
    * Arreglos
    
    * fixesHOla

--- client/src/components/Newsletter.jsx
+++ client/src/components/Newsletter.jsx
@@ -1,12 +1,9 @@
-import { Send } from "@material-ui/icons";
-import React,{ useState } from "react";
-import styled from "styled-components";
-import { mobile } from "../responsive";
-import { newsletter} from "../store/actions";
-import {useDispatch, useSelector} from "react-redux"
-
-
-
+import { Send } from '@material-ui/icons'
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import { mobile } from '../responsive'
+import { newsletter } from '../store/actions'
+import { useDispatch, useSelector } from 'react-redux'
 
 const Container = styled.div`
   height: 60vh;
@@ -20,22 +17,20 @@ const Container = styled.div`
   @media (min-width: 320px) {
     flex-direction: column;
     padding-right: 5px;
-  
   }
-  
-`;
+`
 
 const Title = styled.h1`
   font-size: 70px;
   margin-bottom: 20px;
-`;
+`
 
 const Desc = styled.div`
   font-size: 24px;
   font-weight: 300;
   margin-bottom: 20px;
-  ${mobile({ textAlign: "center" })}
-`;
+  ${mobile({ textAlign: 'center' })}
+`
 
 const InputContainer = styled.div`
   width: 50%;
@@ -44,115 +39,95 @@ const InputContainer = styled.div`
   display: flex;
   justify-content: space-between;
   border: 1px solid lightgray;
-  ${mobile({ width: "80%" })}
-`;
+  ${mobile({ width: '80%' })}
+`
 
 const Input = styled.input`
   border: none;
   flex: 8;
   padding-left: 20px;
-`;
+`
 
 const Button = styled.button`
   flex: 1;
   border: none;
-  background-color: ${props => props.disabled?"#dbd3f7" : "blue"};
+  background-color: ${props => (props.disabled ? '#dbd3f7' : 'blue')};
   color: white;
-`;
+`
 
 const Paragraph = styled.p`
-   color: red;
-   font-size: 15px;
-   font-weight: 3;
-`;
+  color: red;
+  font-size: 15px;
+  font-weight: 3;
+`
 
-export function validate(input) {
-    let errors = {};
+export function validate (input) {
+  let errors = {}
 
-    if (!input.email) {
-        errors.mail = 'Correo Requerido';
-    } else if (!/\S+@\S+\.\S+/.test(input.email)) {
-        errors.mail = 'Correo Invalido';
-    }
+  if (!input.email) {
+    errors.mail = 'Correo Requerido'
+  } else if (!/\S+@\S+\.\S+/.test(input.email)) {
+    errors.mail = 'Correo Invalido'
+  }
 
-    if (Object.keys(errors).length === 0) {
-        errors.active = false
-    }
-
-    else errors.active = true
-    return errors;
-};
+  if (Object.keys(errors).length === 0) {
+    errors.active = false
+  } else errors.active = true
+  return errors
+}
 
 const Newsletter = () => {
-
   const dispatch = useDispatch()
   const token = useSelector(state => state.token)
-  
 
- 
-    
-    const [errors, setErrors] = useState({});
-    const [input, setInput] = useState(
-        {email:""}
-    );
+  const [errors, setErrors] = useState({})
+  const [input, setInput] = useState({ email: '' })
 
-    const handleInputChange = function (e) {
-        e.preventDefault()
-        setInput({
-            ...input,
-            [e.target.name]: e.target.value
-        });
+  const handleInputChange = function (e) {
+    e.preventDefault()
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value
+    })
 
-        setErrors(validate({
-            ...input,
-            [e.target.name]: e.target.value
-        }));
-    }
-        const handleClick = e => {
-          e.preventDefault()
-          dispatch(newsletter(input,token))
-          alert("Gracias por tu suscripcion.")
-          window.scrollTo(0, 0)
-          
-         
-          
-        }
-
-        
-                  
+    setErrors(
+      validate({
+        ...input,
+        [e.target.name]: e.target.value
+      })
+    )
+    setInput({
+      [e.target.name]: ' '
+    })
+  }
+  const handleClick = e => {
+    e.preventDefault()
+    dispatch(newsletter(input, token))
+    alert('Gracias por tu suscripcion.')
+    window.location.reload()
+    window.scrollTo(0, 0)
+  }
 
   return (
     <Container>
       <Title>Noticias</Title>
       <Desc>Suscribite y enterate de todos lo que logramos con su ayuda</Desc>
-          <InputContainer>
-              
-              <Input onChange={(e) => handleInputChange(e)}
-                  type='email'
-                  name='email'
-                  placeholder="Tu email..."
-                 
-              />
-              
+      <InputContainer>
+        <Input
+          onChange={e => handleInputChange(e)}
+          type='email'
+          name='email'
+          placeholder='Tu email...'
+          
+        />
 
-    
-              <Button disabled={errors.active} onClick={handleClick}>
-
-            
-
-                  <Send />
-              </Button>
-
-              
-        
-          </InputContainer>
-          {errors.mail && (
-              <Paragraph>{errors.mail}</Paragraph>
-          )}
+        <Button disabled={errors.active} onClick={handleClick}>
+          <Send />
+        </Button>
+      </InputContainer>
+      {errors.mail && <Paragraph>{errors.mail}</Paragraph>}
     </Container>
-  );
-};
+  )
+}
 
-export default Newsletter;
-
-  
+export default Newsletter
